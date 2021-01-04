21bd011f0e55a328f112b178d222311b48533d18
lwrnc788 <37734971+lwrnc788@users.noreply.github.com>
2021-01-04 10:08:48 +0800

    added pixelselector
    this uses a MapImage class to choose n random pixels from a given image, excluding (#000000)

---
+++ pixelselector/pixelselector.py
@@ -0,0 +1,50 @@
+import numpy as np
+from PIL import Image
+import random
+
+class MapImage:
+    def __init__(self, width_px, height_px, img_url):
+        self.width = width_px
+        self.height = height_px
+        
+        self.imgurl = img_url
+        self.imgobj = Image.open(self.imgurl).convert('RGB')
+        
+        self.imgarray1D = np.array(self.imgobj.getdata())
+        self.imgarray2D = np.array(self.imgobj)
+
+    def show2DArray(self):
+        print(self.imgarray2D)
+
+    def show2DPixel(self, x, y):
+        print(self.imgobj.getpixel((x,y)))
+
+    def getXYFrom1Dpixel(self, i):
+        x = i % self.width
+        y = i // self.width
+        return (x,y)
+    
+    def selectRandomPixels(self, numpixels):
+        validpixels = []
+        
+        for i,pixel in enumerate(self.imgarray1D):
+            isPixelWhite = (pixel == [0,0,0]).all()
+            # check all list elements for if pixel is exactly [0,0,0]
+            
+            if not isPixelWhite:
+                validpixels.append((pixel, self.getXYFrom1Dpixel(i)))
+
+        selectedpixels = random.sample(validpixels, numpixels)
+
+        for pixel in selectedpixels:
+            print(pixel[1])
+
+  
+print('--- Metro Manila ---')
+MetroManila = MapImage(19, 45, 'metromanila_pixelated.png')
+# MetroManila.show2DArray()
+MetroManila.selectRandomPixels(25)
+print()
+
+
+print('--- Metro *** ---')
