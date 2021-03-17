d127a8e8404376ae9247e022d1c3380f8106395b
pshs22-dominic-lawrence-bermudez <56672015+pshs22-dominic-lawrence-bermudez@users.noreply.github.com>
2021-03-18 03:53:07 +0800

    rearranged isolated parts of code based on flow
    - converted KFold divisions to actual code
    - fixed more explanatory comments

--- main.py
+++ main.py
@@ -1,48 +1,19 @@
-import tensorflow as tf
+import img_functions as img_funcs
 
+import tensorflow as tf
+from sklearn.model_selection import KFold
 import numpy as np
+import PIL
+
 import os
 import cv2
 
-import PIL
+#--- ----- CNN Parameters
 
-#--- -----
+### image parameters
 
-# display image/s using matplotlib.pyplot
-# https://www.tensorflow.org/tutorials/images/segmentation
-
-from matplotlib import pyplot as plt
-
-def display(display_list):
-    plt.figure(figsize=(15, 15))
-
-    for i in range(len(display_list)):
-        plt.subplot(1, len(display_list), i+1)
-        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
-        #plt.axis('off')
-    plt.show()
-
-#--- ----- Prerequisite functions for the CNN
-    
-### create list of all images inside a folder path + its subfolders
-    
-def get_all_images_rgb(file_path):
-    images_rgb = []
-    
-    for roots,dirs,files in os.walk(file_path):
-        for fn in files:
-            # check each filename if it falls under any of the valid datatypes
-            if fn.endswith('.jpg') or fn.endswith('.png'):
-                image_path = os.path.join(roots,fn)
-                image = cv2.imread(image_path)
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                images_rgb.append(image)
-
-    return images_rgb
-
-#---
-
-### True Mask to Land Use conversion (numerical)
+img_HEIGHT = 480
+img_WIDTH = 480
 
 lookup_rgb_to_index = {
     (97,64,31): 0,	    # agricultural - brown - #61401F
@@ -56,110 +27,39 @@ lookup_rgb_to_index = {
     # (XX,XX,XX): 7 = not found/unclassified
 }
 
-def pixel_rgb_to_index(rgb_list):
-    # converts a 3x1 list [R, G, B] into a 3x1 tuple (R, G, B)
-    # the tuple is then processed in the lookup dictionary
-    #       to get a single number for the actual classification
-    
-    return lookup_rgb_to_index.get(tuple(rgb_list), len(lookup_rgb_to_index))
-
-def image_rgb_to_index(rgb_image):
-    # converts a HEIGHTxWIDTHx3 (in this case 480x480) tensor into
-    #       a HEIGHTxWIDTH 2D list
-    index_image = []
-
-    for row_of_pixels in rgb_image:
-        current_row = []
-        
-        for pixel in row_of_pixels:
-            current_row.append(pixel_rgb_to_index(pixel))
-
-        index_image.append(current_row)            
-
-    return index_image
-
 #---
 
-### Image Processing for making CNN Datasets
+### model interation parameters
 
-def load_image_train(datapoint):
-    # datapoint is just some iterable with both an image an a segmentation mask
-    # examples from tensorflow use iterable classes for these
-    # the later functions will instantiate tuples for the datapoints
-    
-    src_image = datapoint['image']
-    src_mask = datapoint['segmentation_mask']
-    input_image = tf.image.resize(src_image, (img_HEIGHT, img_WIDTH))
-    input_mask = tf.image.resize(src_mask, (img_HEIGHT, img_WIDTH))
-
-    #augment_data(input_image, input_mask)
-
-    input_image, input_mask = normalize(input_image, input_mask)
-
-    return input_image, input_mask
-
-def load_image_test(datapoint):
-    # see above re: datapoints
-    
-    src_image = datapoint['image']
-    src_mask = datapoint['segmentation_mask']
-    input_image = tf.image.resize(src_image, (img_HEIGHT, img_WIDTH))
-    input_mask = tf.image.resize(src_mask, (img_HEIGHT, img_WIDTH))
-
-    input_image, input_mask = normalize(input_image, input_mask)
-
-    return input_image, input_mask
-
-def normalize(image, mask):
-    # divides the RGB values of the image by 255
-    # changes the range of values in the tensor from 0-255 into 0-1
-    image = tf.cast(image, tf.float32) / 255.0
-
-    return image, mask
-
-
-##define dataAugmentation:
-##    let x be a random number from [0,1]
-##    if > 0.5:
-##        flip the image
-##    else:
-##        rotate the image by a random degree
-##
-
-def augment_data(image, mask):
-    if tf.random.uniform(()) > 0.5:
-        input_image = tf.image.flip_left_right(image)
-        input_mask = tf.image.flip_left_right(mask)
-        pass
-
-    # todo : clarify if do all 4 rotations from 0 to 270?
-    # still needs fixing with regards to actually increasing the number of images
-
-#--- ----- The actual CNN
-
-img_HEIGHT = 480
-img_WIDTH = 480
-
-TRAIN_SIZE = 2500
+TRAIN_SIZE = 2500                                   
 FOLDS = 5
+BATCH_SIZE = 64                                     # power of 2
+STEPS_PER_EPOCH = -(-TRAIN_SIZE // BATCH_SIZE)      # ceiling division
+
+EPOCHS = 727                                        # filler number, just has to be more than enough to overfit before reaching the final epoch
+
 TEST_SIZE = 500
 
-BATCH_SIZE = 64                             # power of 2
-ITERATIONS = -(-TRAIN_SIZE // BATCH_SIZE)   # ceiling division
-EPOCHS = 727                                # filler number, just has to be more than enough to overfit before reaching the final epoch
-
 #---
+
+### model implementation
+
+ACTIVATION = 'relu'
+OPTIMIZER = 'sgd'                                           # Stochastic Gradient Descent
+LOSS = tf.keras.losses.SparseCategoricalCrossentropy()      # Sparce Categorical Cross-Entropy
+
+#--- ----- CNN Implementation
 
 ### Training + Validation Sets
 
 train_images_DIR = os.path.join('train_imgdata', 'satellite_4800x4800')
 train_truemasks_DIR = os.path.join('train_imgdata', 'truemask_480x480')
 
-train_images = get_all_images_rgb(train_images_DIR)
-train_truemasks_color = get_all_images_rgb(train_truemasks_DIR)
+train_images = img_funcs.get_all_images_rgb(train_images_DIR)
+train_truemasks_color = img_funcs.get_all_images_rgb(train_truemasks_DIR)
 train_truemasks_index = []
 
-train_samples = list(zip(train_images, train_images))
+train_dataset = list(zip(train_images, train_images))
     # (image, mask) tuple x 2500
 
     # pray that get_all_files() preserves the image order when applied
@@ -167,10 +67,34 @@ train_samples = list(zip(train_images, train_images))
 
     # todo: remember to set to (train_images, train_truemasks_index) eventually
 
+kf = KFold(n_splits=FOLDS, shuffle=True)
+train_dataset_folds = kf.n_splits
 
-print(image_rgb_to_index(train_truemasks_color[1]))
+list_100 = [i for i in range(100)]
+list_100_folds = kf.n_splits(list_100)
 
+for f in range(FOLDS):
+    current_trainset = list_100_folds[0]
+    current_valset = list_100_folds[1]
 
+    print(current_valset)
+    
+    for e in range(EPOCHS):
+        #build
+        pass
+
+    ##    model = Sequential()
+    ##    model.add(
+    ##
+    ##    model.compile(
+    ##        optimizer='sgd',                                        # stochastic gradient descent
+    ##        loss=tf.keras.losses.SparseCategoricalCrossentropy(),   # sparse categorical cross-entropy
+    ##        metrics=[
+    ##            tf.keras.metrics.Accuracy(),
+    ##            tf.keras.metrics.Precision(),
+    ##            tf.keras.metrics.Recall()
+    ##        ]
+    ##    )
 #---
 
 ### Test Set
@@ -178,39 +102,19 @@ print(image_rgb_to_index(train_truemasks_color[1]))
 test_images_DIR = os.path.join('test_imgdata', 'satellite_480x480')
 test_truemasks_DIR = os.path.join('test_imgdata', 'truemask_480x480')
 
-test_images = get_all_images_rgb(test_images_DIR)
-test_truemasks_color = get_all_images_rgb(test_truemasks_DIR)
-test_truemasks_index = []
+test_images = img_funcs.get_all_images_rgb(test_images_DIR)
+test_truemasks_color = img_funcs.get_all_images_rgb(test_truemasks_DIR)
+test_truemasks_index = [image_rgb_to_index(img) for img in test_truemasks_color]
 
-test_samples = list(zip(test_images, test_images))
+test_dataset = list(zip(test_images, test_images))
     # (image, mask) tuple x 500
 
     # same prayers as above with the Training/Validation Set
-# todo: remember to set to (test_images, test_truemasks_index) eventually
-    
-#---
-
-### sample segmentation dataset from tensorflow
-
-##import tensorflow_datasets as tfds
-##
-##dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
-##
-##train = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
-##s = train.take(1)
-##
-##for sample in s:
-##    print(sample[1].numpy())
-##    print(sample[1][240][240])      # pet = 1
-##    print(sample[1][0][0])          # background = 2
-##    print(sample[1][200][100])      # border = 3
-##
-##    display([sample[0], sample[1]])
-##    pass
+    # todo: remember to set to (test_images, test_truemasks_index) eventually
 
 #---
 
-def buildCNN():
+def buildCNN(dataset):
     pass
 
 ##define buildCNN:
@@ -242,24 +146,49 @@ def buildCNN():
 ##    display input image, true mask, and predicted mask
 ##
 
-#--- ----- leftover pseudocode
+# ---
 
-##import images and color maps as tf dataset
+##base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
 ##
-##//color maps would have pixels that are labeled from 0-6
-##divide the rgb values of the images by 255
+### Use the activations of these layers
+##layer_names = [
+##    'block_1_expand_relu',   # 64x64
+##    'block_3_expand_relu',   # 32x32
+##    'block_6_expand_relu',   # 16x16
+##    'block_13_expand_relu',  # 8x8
+##    'block_16_project',      # 4x4
+##]
+##base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
 ##
-##//used to ensure that the rgb values are within [0,1]
+### Create the feature extraction model
+##down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
 ##
-##divide the dataset into training dataset and testing dataset
-##use data augmentation on the training dataset
-##
-##divide the training dataset into five for k-fold cross validation
-##for validationSet in trainingDataset:
-##    for e from 1 to epoch:
-##    build CNN using buildCNN
-##train the model using the four training datasets, leaving the validationSet to act as a validation set
-##
+##down_stack.trainable = False
+
+#--- ----- drafted pseudocode
+
+    #+# import images and color maps as tf dataset
+
+    #+# //color maps would have pixels that are labeled from 0-6
+    #+# divide the rgb values of the images by 255
+    #+# 
+    #+# //used to ensure that the rgb values are within [0,1]
+
+    #+# divide the dataset into training dataset and testing dataset
+
+    ##use data augmentation on the training dataset
+
+    #+# divide the training dataset into five for k-fold cross validation
+
+    #+# for validationSet in trainingDataset:
+    #+#     for e from 1 to epoch:
+    ##          build CNN using buildCNN
+    ##          train the model using the four training datasets, leaving the validationSet to act as a validation set
+
+    #+# for f from 0 to 4:
+    #+#     current_valid = folds[f]
+    #+#     current_train = everything else
+
 ##    //loss function will be sparse categorical cross entropy
 ##    evaluate the model using the validation set
 ##    record evaluation
