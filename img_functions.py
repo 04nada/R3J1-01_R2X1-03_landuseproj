ffe04cc4a981ff9c91374f7237fc3fef19075c39
pshs22-dominic-lawrence-bermudez <56672015+pshs22-dominic-lawrence-bermudez@users.noreply.github.com>
2021-03-18 06:42:32 +0800

    annotated the functions with datatypes

--- img_functions.py
+++ img_functions.py
@@ -1,7 +1,4 @@
 import tensorflow as tf
-from sklearn.model_selection import KFold
-import numpy as np
-import PIL
 
 #--- ----- Image Displaying
 
@@ -10,7 +7,7 @@ import PIL
 
 from matplotlib import pyplot as plt
 
-def display_images(display_list):
+def display_images(display_list: list):
     plt.figure(figsize=(15, 15))
 
     for i in range(len(display_list)):
@@ -19,7 +16,7 @@ def display_images(display_list):
         #plt.axis('off')
     plt.show()
 
-def print_image(image):
+def print_image(image: list):
     for row_of_pixels in image:
         print(row_of_pixels)
 
@@ -30,7 +27,7 @@ def print_image(image):
 import os
 import cv2
 
-def get_all_image_names(file_path):
+def get_all_image_names(file_path: str):
     image_names = []
     
     for roots,dirs,files in os.walk(file_path):
@@ -44,7 +41,7 @@ def get_all_image_names(file_path):
 
     return images_rgb
 
-def get_all_images_rgb(file_path):
+def get_all_images_rgb(file_path: str):
     images_rgb = []
     
     for roots,dirs,files in os.walk(file_path):
@@ -62,14 +59,14 @@ def get_all_images_rgb(file_path):
 
 ### convert True Mask to Land Use (numerical)
 
-def pixel_rgb_to_index(rgb_list, lookup_table):
+def pixel_rgb_to_index(rgb_list: list, lookup_table: dict):
     # converts a 3x1 list [R, G, B] into a 3x1 tuple (R, G, B)
     # the tuple is then processed in the lookup dictionary
     #       to get a single number for the actual classification
     
     return lookup_table.get(tuple(rgb_list), len(lookup_table))
 
-def image_rgb_to_index(rgb_image, lookup_table):
+def image_rgb_to_index(rgb_image: list, lookup_table: dict):
     # converts a HEIGHTxWIDTHx3 (in this case 480x480) tensor into
     #       a HEIGHTxWIDTH 2D list
     index_image = []
@@ -88,10 +85,10 @@ def image_rgb_to_index(rgb_image, lookup_table):
 
 ### compile images and masks to fit with CNN
 
-def load_image_train(datapoint):
+def load_image_train(datapoint: tuple):
     # datapoint is just some iterable with both an image and a segmentation mask
     # examples from tensorflow use iterable classes for these
-    # the later functions will instantiate tuples for the datapoints
+    # datapoint is usually a tuple as (image, mask)
     
     src_image = datapoint['image']
     src_mask = datapoint['segmentation_mask']
@@ -104,7 +101,7 @@ def load_image_train(datapoint):
 
     return input_image, input_mask
 
-def load_image_test(datapoint):
+def load_image_test(datapoint: tuple):
     # see above re: datapoints
     
     src_image = datapoint['image']
@@ -116,7 +113,7 @@ def load_image_test(datapoint):
 
     return input_image, input_mask
 
-def normalize(image, mask):
+def normalize(image: list, mask: list):
     # divides the RGB values of the image by 255
     # changes the range of values in the tensor from 0-255 into 0-1
     image = tf.cast(image, tf.float32) / 255.0
