d953236d73bdf3bbbf5851f30d6e7cd695a01139
pshs22-dominic-lawrence-bermudez <56672015+pshs22-dominic-lawrence-bermudez@users.noreply.github.com>
2021-04-20 01:06:41 +0800

    integrated image sorting with image file generation
    - added a visual print() indicator to generate_grid_image_files() to properly see working progress of the function, while the Python Shell would normally appear "frozen"
    - ported every path instance from os.path strings into pathlib.Path objects
    - modified the Image Gridifier + Image File Generator to accept labels as inputs, and thus sort subimages into folders
    - changed the functions to use interchangeable, named arguments as parameters, instead of some arbitrary order of variables

--- image_gridifier/image_gridifier.py
+++ image_gridifier/image_gridifier.py
@@ -1,50 +1,71 @@
 import numpy as np
 
 from PIL import Image
-import os
 import cv2
 from pathlib import Path
 import shutil
 
 # ---
 
-# sample matrices
+lookup_rgb_to_index = {
+    (97,64,31): 0,	    # agricultural - brown - #61401F
+    (160,32,239): 1,	    # commercial - purple - #A020EF
+    (221,190,170): 2,       # industrial - beige - #DDBEAA
+    (238,0,2): 3,   	    # institutional - red - #ED0000
+    (45,137,86): 4,	    # recreational - green - #2D8956
+    (254,165,0): 5,	    # residential - yellow - #FEA500
+    (0,0,88): 6		    # transport - dark blue - #000057
+}
 
-small_sample = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
-
-large_sample = []
-for i in range(3000):
-    large_sample.append([i*3000+j for j in range(1000)])
+label_names = [
+    'agricultural',
+    'commercial',
+    'industrial',
+    'institutional',
+    'recreational',
+    'residential',
+    'transport'
+]
 
 # ---
 
-# creates a folder containing subimages that would have been generates
-#       from gridify_image() with the same parameters
-# this function accepts image path, instead of an image matrix
-def generate_grid_image_files(src_image_path:str, grid_HEIGHT:int, grid_WIDTH:int, is_last_row_bigger=False, is_last_column_bigger=False):
-    # get filename (no extension) and directory name of src_image from path
-    src_image_filename = Path(src_image_path).stem
-    src_image_directory = os.path.dirname(src_image_path)
+def generate_grid_image_files(grid_HEIGHT:int, grid_WIDTH:int, is_last_row_bigger=False, is_last_column_bigger=False,
+*, image_abspath:str):
+    # creates a folder containing subimages that would have been generated
+    #       from gridify_image() with the same parameters
+    # this function accepts <image_abspath>, instead of an image matrix
 
-    # create empty folder with same name as src_image, on the same level
-    subimage_directory = os.path.join(src_image_directory, src_image_filename)
-    
-    if os.path.exists(subimage_directory):
-        # if folder already exists, delete it
-        shutil.rmtree(subimage_directory)
-        
-    os.mkdir(subimage_directory)
+    print('=== Unsorted Image Gridifier - start ===')
 
     # ---
 
-    # load src_image as numpy array
-    src_image = cv2.imread(src_image_path)
-    src_image = cv2.cvtColor(src_image, cv2.COLOR_BGR2RGB)
+    ### Instantiate the source image variables
 
-    # convert src_image from numpy array to list
-    src_image = np.array(src_image).tolist()
+    src_image_abspath = image_abspath
+    src_image_directory = Path(src_image_abspath).parent
+    src_image_filename = Path(src_image_abspath).stem
+
+    # create empty folder with same name as src_image, on the same level
+    subimage_directory = src_image_directory / src_image_filename
+
+    print('- for: ' + src_image_filename)
+    if subimage_directory.exists():
+        # if folder already exists, delete it
+        shutil.rmtree(subimage_directory)
+        
+    subimage_directory.mkdir()
+
+    # ---
+
+    ### Gridify the source image
+
+    # load src_image as numpy array
+    src_image = cv2.imread(src_image_abspath)
+    src_image = cv2.cvtColor(src_image, cv2.COLOR_BGR2RGB)
     
     gridified_image = gridify_image(src_image, grid_HEIGHT, grid_WIDTH, is_last_row_bigger, is_last_column_bigger)
+
+    # ---
     
     for gr,grid_row in enumerate(gridified_image):
         for subimg,subimage in enumerate(grid_row):
@@ -55,15 +76,95 @@ def generate_grid_image_files(src_image_path:str, grid_HEIGHT:int, grid_WIDTH:in
                 + str(gr+1)         # row number, 1-?
             )
 
-            fileroot = os.path.join(subimage_directory, filename)
+            file_abspath = subimage_directory / filename
             
             # save subimage in previously made folder
             current_image = Image.fromarray(np.array(subimage).astype(np.uint8))
-            current_image.save(fileroot+'.png', format='png')
+            current_image.save(file_abspath.with_suffix('.png'), format='png')
 
-# generate matrix of subimage matrices from a larger image matrix
-# divides the larger image into grid by given height and width
+        print('-- Finished: Row ' + str(gr+1) + ' of ' + str(len(gridified_image)) + ' --')
+
+    print('=== Unsorted Image Gridifier - finish ===')
+
+  
+def generate_sorted_grid_image_files(grid_HEIGHT:int, grid_WIDTH:int, is_last_row_bigger=False, is_last_column_bigger=False,
+*, image_abspath:str, labels_abspath:str, lookup_colors:dict, label_names:list):
+    # also uses the <labels_abspath>, <lookup_colors>, and <label_names> keyowrds
+    #       to indicate the image with the corresponding label colors
+    
+    print('=== Sorted Image Gridifier - start ===')
+
+    # ---
+
+    ### Instantiate the source image variables
+
+    src_image_abspath = image_abspath
+    src_image_directory = Path(src_image_abspath).parent
+    src_image_filename = Path(src_image_abspath).stem
+
+    # create empty folder with same name as src_image, on the same level
+    subimage_directory = src_image_directory / src_image_filename
+
+    print('- for: ' + src_image_filename)
+    if subimage_directory.exists():
+        # if folder already exists, delete it, to force empty folder
+        shutil.rmtree(subimage_directory)
+        
+    subimage_directory.mkdir()
+
+    # create empty subfolder for each class/label
+    for label in label_names:
+        class_directory = subimage_directory / label
+
+        if not class_directory.exists():
+            class_directory.mkdir()
+    
+    # ---
+
+    ### Process and gridify the source image
+    
+    src_image = cv2.imread(src_image_abspath)
+    src_image = cv2.cvtColor(src_image, cv2.COLOR_BGR2RGB)
+
+    gridified_image = gridify_image(src_image, grid_HEIGHT, grid_WIDTH, is_last_row_bigger, is_last_column_bigger)
+
+
+    ### Process the labels image
+    
+    labels_image = cv2.imread(labels_abspath)
+    labels_image = cv2.cvtColor(labels_image, cv2.COLOR_BGR2RGB)
+    
+    # ---
+    
+    for gr,grid_row in enumerate(gridified_image):
+        for subimg,subimage in enumerate(grid_row):
+            # identify the class name given the class color
+            class_color = tuple(labels_image[gr][subimg])
+            class_number = lookup_colors.get(class_color)
+            class_name = label_names[class_number]
+            
+            # append LETTERnumber code for each subimage
+            filename = (src_image_filename
+                + ' '
+                + chr(65+subimg)    # column letter, A-?
+                + str(gr+1)         # row number, 1-?
+            )
+
+            file_abspath = subimage_directory / class_name / filename
+            
+            # save subimage in previously made folder
+            current_image = Image.fromarray(np.array(subimage).astype(np.uint8))
+            current_image.save(file_abspath.with_suffix('.png'), format='png')
+
+        print('-- Finished: Row ' + str(gr+1) + ' of ' + str(len(gridified_image)) + ' --')
+
+    print('=== Sorted Image Gridifier - finish ===')
+
+
 def gridify_image(image:list, grid_HEIGHT:int, grid_WIDTH:int, is_last_row_bigger=False, is_last_column_bigger=False):
+    # generate matrix of subimage matrices from a larger image matrix
+    # divides the larger image into grid by given height and width
+    
     # last two parameters determine if last row/column will be bigger or smaller,
     #       if the grid lengths do not divide the image lengths evenly
 
@@ -92,49 +193,53 @@ def gridify_image(image:list, grid_HEIGHT:int, grid_WIDTH:int, is_last_row_bigge
     print("GRID ROWS: " + str(NUM_ROWS))
 
     # O(n^4) nested lists end me now
-    for gr in range(NUM_ROWS):
+    for g_r in range(NUM_ROWS):
         grid_row = []
-
-        for gc in range(NUM_COLUMNS):
-            subimage = []
-
-            # compute subimage height based on row number and
-            #       if the last row is bigger or smaller than the rest
-            if gr < NUM_ROWS-1:
-                # subimage uses standard grid height for every row except the last
+        
+        # compute subimage height based on row number and
+        #       from if the last row is bigger or smaller than the rest
+        if g_r < NUM_ROWS-1:
+            # subimage uses standard grid height for every row except the last
+            subimage_HEIGHT = grid_HEIGHT
+        else:
+            if image_HEIGHT % grid_HEIGHT == 0:
+                # if the grid height evenly divides image height
+                #       then use that standard grid height for the last row anyway
                 subimage_HEIGHT = grid_HEIGHT
             else:
-                if image_HEIGHT % grid_HEIGHT == 0:
-                    # if the grid height evenly divides image height
-                    #       then use that standard grid height for the last row anyway
-                    subimage_HEIGHT = grid_HEIGHT
+                if is_last_row_bigger:
+                    subimage_HEIGHT = (image_HEIGHT % grid_HEIGHT) + grid_HEIGHT
                 else:
-                    if is_last_row_bigger:
-                        subimage_HEIGHT = (image_HEIGHT % grid_HEIGHT) + grid_HEIGHT
-                    else:
-                        subimage_HEIGHT = image_HEIGHT % grid_HEIGHT
+                    subimage_HEIGHT = image_HEIGHT % grid_HEIGHT
 
-            for ir in range(subimage_HEIGHT):
-                subimage_row = []
+        # ---
+        
+        for g_c in range(NUM_COLUMNS):
+            subimage = []
 
-                # compute subimage width based on column number and
-                #       if the last row is bigger or smaller than the rest
-                if gc < NUM_COLUMNS-1:
-                    # subimage uses standard grid width for every column except the last
+            # compute subimage width based on column number and
+            #       from if the last row is bigger or smaller than the rest
+            if g_c < NUM_COLUMNS-1:
+                # subimage uses standard grid width for every column except the last
+                subimage_WIDTH = grid_WIDTH
+            else:
+                if image_WIDTH % grid_WIDTH == 0:
+                    # if the grid width evenly divides image width
+                    #       then use that standard grid width for the last column anyway
                     subimage_WIDTH = grid_WIDTH
                 else:
-                    if image_WIDTH % grid_WIDTH == 0:
-                        # if the grid width evenly divides image width
-                        #       then use that standard grid width for the last column anyway
-                        subimage_WIDTH = grid_WIDTH
+                    if is_last_column_bigger:
+                        subimage_WIDTH = (image_WIDTH % grid_WIDTH) + grid_WIDTH
                     else:
-                        if is_last_column_bigger:
-                            subimage_WIDTH = (image_WIDTH % grid_WIDTH) + grid_WIDTH
-                        else:
-                            subimage_WIDTH = image_WIDTH % grid_WIDTH
-                        
-                for ic in range(subimage_WIDTH):
-                    subimage_pixel = image[gr*grid_HEIGHT + ir][gc*grid_WIDTH + ic]
+                        subimage_WIDTH = image_WIDTH % grid_WIDTH
+            
+            # ---
+            
+            for si_r in range(subimage_HEIGHT):
+                subimage_row = []
+  
+                for si_c in range(subimage_WIDTH):
+                    subimage_pixel = image[g_r*grid_HEIGHT + si_r][g_c*grid_WIDTH + si_c]
                     
                     subimage_row.append(subimage_pixel)
 
