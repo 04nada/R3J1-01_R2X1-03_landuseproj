09b16f45c47aff65d88d25b3289d224982c6bdf7
pshs22-dominic-lawrence-bermudez <56672015+pshs22-dominic-lawrence-bermudez@users.noreply.github.com>
2021-04-20 01:05:06 +0800

    structural changes to the Datapoint class and its container
    - implemented a Dataset class which assumes that the data in a folder is already perfectly sorted by class
    - revised the Datapoint class to only use names and numbers instead of the full label color per instance

--- class_datapoint.py
+++ class_datapoint.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 import numpy as np
 
 from PIL import Image
@@ -28,116 +29,98 @@ label_names = [
     'transport'
 ]
 
-class LargeDatapoint:
-    def __init__(self, imgpath:str):
-        self.image_path = imgpath
+class Dataset:
+    ### Initialization
+    
+    def __init__(self, *, directory, **kwargs):
+        self.__directory = Path(directory)
+        self.__subdirectories = []
+        
+        self.__classes = kwargs.get('classes', None)
 
-        # get filename (no extension) and directory name of image from path
-        self.image_filename = Path(self.image_path).stem
-        self.image_directory = os.path.dirname(self.image_path)
-        self.image_extension = Path(self.image_path).suffix
+        self.__datapoints = []
         
         # ---
-        
-        # load src_image as numpy array
-        self.image_nptensor = cv2.imread(self.image_path)
-        self.image_nptensor = cv2.cvtColor(self.image_nptensor, cv2.COLOR_BGR2RGB)
 
-    def init_labels(self, lblpath:str, lbllookup_rgb2i:dict, lbllookup_i2n:list):
-        self.labels_path = lblpath
-
-        self.labels_filename = Path(self.labels_path).stem
-        self.labels_directory = os.path.dirname(self.labels_path)
-        self.labels_extension = ".png"
-        self.labels_fileformat = "png"
-
-        self.label_lookup_rgb_to_index = lbllookup_rgb2i
-        self.label_lookup_index_to_name = lbllookup_i2n
-
-        # ---
-
-        self.labels_nptensor = cv2.imread(self.image_path)
-        self.labels_nptensor = cv2.cvtColor(self.image_nptensor, cv2.COLOR_BGR2RGB)
-
-    def set_datapoints(self, imgdir_sortbyclass:str, grid_HEIGHT:int, grid_WIDTH:int, is_last_row_bigger=False, is_last_column_bigger=False):
-        gridified_image_tensor = image_gridifier.gridify_image(self.image_nptensor, grid_HEIGHT, grid_WIDTH, is_last_row_bigger, is_last_column_bigger)
-        gridified_label_tensor = image_gridifier.gridify_image(self.labels_nptensor, grid_HEIGHT, grid_WIDTH, is_last_row_bigger, is_last_column_bigger)
-
-        self.datapoints = []
-
-        for gr,grid_row in enumerate(gridified_image_tensor):
-            for subimg,subimage in enumerate(grid_row):
-                filename = (self.image_filename
-                    + ' '
-                    + chr(65+subimg)    # column letter, A-?
-                    + str(gr+1)         # row number, 1-?
-                )
-
-                # initialize a datapoint class
-                current_datapoint = Datapoint()
-                current_datapoint.init_label(
-                    gridified_label_tensor[gr][subimg],
-                    self.label_lookup_rgb_to_index,
-                    self.label_lookup_index_to_name
-                )
-
-                # identify fileroot for datapoint based on its classification as a subfolder
-                fileroot = os.path.join(
-                    imgdir_sortbyclass,
-                    current_datapoint.get_label_name(),
-                    filename + ".png"
-                )
+        # create subfolders if specific classes are specified
+        if self.get_classes():
+            for c in self.get_classes():
+                class_directory = Path(self.get_directory(), c)
                 
-                current_datapoint.init_path(fileroot)
+                if not class_directory.exists():
+                    class_directory.mkdir()
 
-    def create_datapoints(self):
-        for dp in self.datapoints:
-            dp.create_file()
-    
+        # get all immediate subdirectories in main directory
+        for subpath in Path.iterdir(self.directory):
+            if subpath.is_dir():
+                self.add_subdirectory(Path(subpath))
+
+        # ---
+
+        # create datapoints for each object in
+        for sd in self.get_subdirectories():
+            for subpath in Path.iterdir(sd):
+                if subpath.is_file():
+                    dp = Datapoint(subpath, sd)
+                    self.add_datapoint(dp)
+
+    #---
+                    
+    ### Getters
+
+    def get_directory(self):
+        return self.__directory
+
+    def get_subdirectories(self):
+        return self.__subdirectories
+
+    def get_classes(self):
+        return self.__classes
+
+    def get_datapoints(self):
+        return self.__datapoints
+
+    #---
+
+    ### Setters
+
+    def add_subdirectory(self, sd):
+        self.__subdirectories.append(sd)
+
+    def add_datapoint(self, dp):
+        self.__datapoints.append(dp)
+        
 class Datapoint:
-    ### Initializers
+    ### Initialization
     
-    def __init__(self):
-        pass
-    
-    def init_path(self, imgroot:str):
+    def __init__(self, abspath:str, label:str):
         ### Image
         
-        self.image_fileroot = imgroot
+        self.__image_abspath = Path(abspath)
 
-        self.image_filename = Path(self.image_path).stem
+        self.image_filename = self.get_image_path().stem
         self.image_directory = os.path.dirname(self.image_path)
-        self.image_extension = Path(self.image_path).suffix
-        self.image_fileformat = image_extension[1:]
+        self.__image_extension = self.get_image_path().suffix
+        self.__image_fileformat = self.__image_extension[1:]
 
         # ---
 
-        self.image_nptensor = cv2.imread(self.image_fileroot)
-        self.image_nptensor = cv2.cvtColor(self.image_tensor, cv2.COLOR_BGR2RGB)
-    
-    def init_label(self, lblcolor:list, lbllookup_rgb2i:dict, lbllookup_i2n:list):      
-        self.label_color = lblcolor
-        self.label_number = self.label_lookup_rgb_to_index.get(self.label_color)
-        self.label_name = self.label_lookup_index_to_name[self.label_number]
+        self.__image_nptensor = cv2.imread(self.image_root)
+        self.__image_nptensor = cv2.cvtColor(self.image_tensor, cv2.COLOR_BGR2RGB)
 
+        # ---
+
+        self.__label_name = label
+        
     # ---
 
     ### Getters
+
+    def get_image_path(self):
+        return self.__image_abspath
     
     def get_image_tensor(self):
-        return self.image_tensor
-
-    def get_label_number(self):
-        return self.label_number
+        return self.__image_nptensor
 
     def get_label_name(self):
-        return self.label_name
-
-    # ---
-
-    def create_file(self):
-        if not os.path.exists(self.image_directory):
-            os.makedirs(self.image_directory)
-
-        img_array = np.array(self.image_tensor).astype(np.uint8)
-        img_array.save(self.image_fileroot + self.image_extension, format=self.image_fileformat)
+        return self.__label_name
