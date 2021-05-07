3ea3863eed50f1c867421aadef4eb7adce204fa6
pshs22-dominic-lawrence-bermudez <56672015+pshs22-dominic-lawrence-bermudez@users.noreply.github.com>
2021-05-08 04:56:41 +0800

    fixed model implementation in line with changes from model_functions
    - changed return function calls to generator function calls
    - stored everything formerly in memory-exhaustive lists, into generators
    --- except the last generators, which have to be reconverted to numpy arrays for model.fit()
    - added some more print lines for progress tracking
    - removed finished/obsolete pseudocode from before
    - started adding test set code similar to the initial training set code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import main_params as mp
-import img_functions as img_funcs
+import model_functions as img_funcs
 
 import tensorflow as tf
 from sklearn.model_selection import KFold
@@ -22,52 +22,72 @@ random.seed(mp.SEED)
 ### Training + Validation Sets
 
 # get training directory wrt current working directory
-train_dataset_directory = Path.cwd() / 'train_imgdata' / 'trueclass_240x240_sortbyclass_full'
+train_dataset_directory = Path.cwd() / 'train_imgdata' / 'trueclass_240x240_sortbyclass_actual'
 
-# create datapoints from dataset directory and label_name list
-train_datapoints = img_funcs.create_datapoints_from_directory(
-    train_dataset_directory.__str__(),
-    mp.label_names_full,
-    normalize=True
+# create datapoint generators from dataset directory and label_name list
+# use the ReGenerator class for reusability
+train_datapoints_regen = img_funcs.ReGenerator(
+    img_funcs.dataset_generator,
+    (train_dataset_directory.__str__(),
+        mp.label_names),
+    {'normalize': True,
+        'n': mp.TRAIN_SAMPLES_PER_CLASS}
 )
-random.shuffle(train_datapoints)
 
-# create k folds of training-validation splits
+# create k folds of random/shuffled training-validation splits
+# KFold follows the numpy seed
 kf = KFold(n_splits=mp.FOLDS, shuffle=True)
-    # KFold follows the numpy seed
-train_datapoints_fold_indices = kf.split(train_datapoints)
-    # kf.split only returns the randomized indices, not the actual sublists
 
-# cleanup
-
-
+# less memory-exhaustive list with the same "length" as the datapoints generator
+train_datapoints_length = train_datapoints_regen.length()
+train_datapoints_filler_list = [i for i in range(train_datapoints_length)]
+# kf.split only returns the randomized indices, not the actual sublists, as a generator of array pairs
+train_datapoints_fold_indices = kf.split(train_datapoints_filler_list)
+    
+       
 ### CNN Training
 
-for f,current_splits in enumerate(train_datapoints_fold_indices):
+models = []
+
+for f in range(mp.FOLDS):
     print('\n=== FOLD ' + str(f+1) + ' of ' + str(mp.FOLDS) + ' - start ===')
 
+    # get indices of training data and validation data for the current fold
+    current_trainset_indices, current_valset_indices = next(train_datapoints_fold_indices)
+
     # get training set datapoints from randomly generated indices
-    current_trainset_indices = current_splits[0]
-    current_trainset_points = [train_datapoints[i] for i in current_trainset_indices]
+    current_trainset_points = img_funcs.ReGenerator(
+        lambda : (datapoint
+            for i,datapoint in enumerate(train_datapoints_regen.gen((), {'log_progress': False}))
+            if i in current_trainset_indices)
+    )
 
     # get validation set datapoints from randomly generated indices
-    current_valset_indices = current_splits[1]
-    current_valset_points = [train_datapoints[i] for i in current_valset_indices]
+    current_valset_points = img_funcs.ReGenerator(
+        lambda : (datapoint
+            for i,datapoint in enumerate(train_datapoints_regen.gen((), {'log_progress': False}))
+            if i in current_valset_indices)
+    )
 
-    # split training set into image and label lists
-    current_train_images = np.array([point[0] for point in current_trainset_points])
-    current_train_labels = [point[1] for point in current_trainset_points]
-    current_train_labels = np.array(current_train_labels)
+    # split training set into image and label generators
+    current_train_images = img_funcs.ReGenerator(
+        lambda : (point[0] for point in current_trainset_points.gen())
+    )
+    current_train_labels = img_funcs.ReGenerator(
+        lambda : (point[1] for point in current_trainset_points.gen())
+    )
 
-    # split validation set into image and label lists
-    current_val_images = [point[0] for point in current_valset_points]
-    current_val_images = np.array(current_val_images)
-    current_val_labels = [point[1] for point in current_valset_points]
-    current_val_labels = np.array(current_val_labels)
-
+    # split validation set into image and label generators
+    current_val_images = img_funcs.ReGenerator(
+        lambda : (point[0] for point in current_valset_points.gen())
+    )
+    current_val_labels = img_funcs.ReGenerator(
+        lambda : (point[1] for point in current_valset_points.gen())
+    )
+    
     # ---
     
-    # only do training augmentation here, so it will happen FOLDSx total
+    # only do training data augmentation here, so it will happen FOLDSx total
     # in order to augment training data but not the validation data, per fold
     # https://stats.stackexchange.com/questions/482787/how-to-do-data-augmentation-and-cross-validation-at-the-same-time
 
@@ -81,43 +101,74 @@ for f,current_splits in enumerate(train_datapoints_fold_indices):
         activation=mp.ACTIVATION,
         input_shape=(mp.img_HEIGHT, mp.img_WIDTH, 3)
     ))
-    
+
+    # continue applying convolutional layers while occasionally doing pooling
     model.add(tf.keras.layers.MaxPooling2D((2, 2)))
     model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu') )
     model.add(tf.keras.layers.MaxPooling2D((2, 2)))
     model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
     
-    # todo: model.add() a lot of shtuff
-
+    # flatten CNN model to a single array of values
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(64, activation=mp.ACTIVATION))
+
+    # final layer corresponds to the total number of classes for classifying into
     model.add(tf.keras.layers.Dense(mp.NUM_CLASSES))
-       
+
+    # compile model using specified tools and metrics
+    print('--- FOLD ' + str(f+1) + ' of ' + str(mp.FOLDS) + ' - model.compile() ---')
     model.compile(
         optimizer=mp.OPTIMIZER,
         loss=mp.LOSS,
         metrics=mp.EVALUATION_METRICS
     )
 
+    # convert final image and label generators to numpy arrays,
+    # so that they get accepted as the model.fit() parameters
+    print('1')
+    current_train_images_array = np.array([image for image in current_train_images.gen()])
+    print('2')
+    current_train_labels_array = np.array([label for label in current_train_labels.gen()])
+    print('3')
+    current_val_images_array = np.array([image for image in current_val_images.gen()])
+    print('4')
+    current_val_labels_array = np.array([image for image in current_val_labels.gen()])
+
+    # fit training and validation to model
+    # also setting other parameters for how the model runs, namely epochs and batch size
+    print('--- FOLD ' + str(f+1) + ' of ' + str(mp.FOLDS) + ' - model.fit() ---')
     model.fit(
-        current_train_images,
-        current_train_labels,
-        validation_data=(current_val_images, current_val_labels),
+        current_train_images_array,
+        current_train_labels_array,
+        validation_data=(current_val_images_array current_val_labels_array),
         epochs = 1,
         batch_size = mp.BATCH_SIZE
     )
 
     model.summary()
-
-    print('=== FOLD ' + str(f+1) + ' of ' + str(mp.FOLDS) + ' - end ===\n')
+    #models.append(model)
+    
+    print('=== FOLD ' + str(f+1) + ' of ' + str(mp.FOLDS) + ' - end ===')
 
 #---
 
 ### Test Set
 
-##test_images_DIR = os.path.join('test_imgdata', 'satellite_480x480')
-##test_truemasks_DIR = os.path.join('test_imgdata', 'truemask_480x480')
-##
+# get testing directory wrt current working directory
+test_dataset_directory = Path.cwd() / 'test_imgdata' / 'trueclass_240x240_sortbyclass_actual'
+
+test_datapoints_regen = img_funcs.ReGenerator(
+    img_funcs.dataset_generator,
+    (test_dataset_directory.__str__(),
+        mp.label_names),
+    {'normalize': True,
+        'n': mp.TEST_SAMPLES_PER_CLASS}
+)
+
 ##test_images = img_funcs.get_all_images_rgb(test_images_DIR)
 ##test_truemasks_color = img_funcs.get_all_images_rgb(test_truemasks_DIR)
 ##test_truemasks_index = [image_rgb_to_index(img) for img in test_truemasks_color]
@@ -127,84 +178,3 @@ for f,current_splits in enumerate(train_datapoints_fold_indices):
 
     # same prayers as above with the Training/Validation Set
     # todo: remember to set to (test_images, test_truemasks_index) eventually
-
-#---
-
-##define buildCNN:
-##    //relu will be used as the activation function for all the layers
-##    //x = max(0, x)
-##    create conv layer 1_1
-##    create conv layer 1_2
-##    max pool output of conv layer 1_2
-##    create conv layer 2_1
-##    create conv layer 2_2
-##    max pool output of conv layer 2_2
-##        ...
-##    create conv layer n_1
-##    create conv layer n_2
-##    max pool output of conv layer n_2
-##    //add layers until overfitting occurs
-##    create conv layer n+1_1
-##    create conv layer n+1_2
-##    upsample the output of conv layer n+1_2
-##        ...
-##    create conv layer 2n_1
-##    create conv layer 2n_2
-##    upsample the output of conv layer 2n_2
-##
-##    flatten the output
-##    create a dense layer
-##    //last layer
-##    convert the tensor into an image
-##    display input image, true mask, and predicted mask
-##
-
-# ---
-
-##base_model = tf.keras.applications.MobileNetV2(
-##    input_shape=[128, 128, 3],
-##    include_top=False
-##)
-##
-### Use the activations of these layers
-##layer_names = [
-##    'block_1_expand_relu',   # 64x64
-##    'block_3_expand_relu',   # 32x32
-##    'block_6_expand_relu',   # 16x16
-##    'block_13_expand_relu',  # 8x8
-##    'block_16_project',      # 4x4
-##]
-##base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
-##
-### Create the feature extraction model
-##down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
-##
-##down_stack.trainable = False
-
-#--- ----- drafted pseudocode
-
-    #+# import images and color maps as tf dataset
-
-    #+# //color maps would have pixels that are labeled from 0-6
-    #+# divide the rgb values of the images by 255
-    #+# 
-    #+# //used to ensure that the rgb values are within [0,1]
-
-    #+# divide the dataset into training dataset and testing dataset
-
-    ##use data augmentation on the training dataset
-
-    #+# divide the training dataset into five for k-fold cross validation
-
-    #+# for validationSet in trainingDataset:
-    #+#     for e from 1 to epoch:
-    ##          build CNN using buildCNN
-    ##          train the model using the four training datasets, leaving the validationSet to act as a validation set
-
-    #+# for f from 0 to 4:
-    #+#     current_valid = folds[f]
-    #+#     current_train = everything else
-
-##    //loss function will be sparse categorical cross entropy
-##    evaluate the model using the validation set
-##    record evaluation
