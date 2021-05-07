d332049f9f48094569c5b2a75f9d9e2cc980805d
pshs22-dominic-lawrence-bermudez <56672015+pshs22-dominic-lawrence-bermudez@users.noreply.github.com>
2021-05-08 04:45:59 +0800

    updated main_gridifier with parameters for generating test set

--- main_gridifier.py
+++ main_gridifier.py
@@ -3,13 +3,15 @@ import main_params as mp
 
 # ---
 
-images = 'D:\\Users\\lwrnc\\Desktop\\LWRNC\\11 - A\\Research 2\\R2X1-03_landuseproj\\train_imgdata\\satellite_4800x4800_unsorted'
-labelses = 'D:\\Users\\lwrnc\\Desktop\\LWRNC\\11 - A\\Research 2\\R2X1-03_landuseproj\\train_imgdata\\colormap_20x20_unsorted'
+train_images = 'D:\\Users\\lwrnc\\Desktop\\LWRNC\\11 - A\\Research 2\\R2X1-03_landuseproj\\train_imgdata\\satellite_4800x4800_unsorted'
+train_labelses = 'D:\\Users\\lwrnc\\Desktop\\LWRNC\\11 - A\\Research 2\\R2X1-03_landuseproj\\train_imgdata\\colormap_20x20_unsorted'
+
+test_images = 'D:\\Users\\lwrnc\\Desktop\\LWRNC\\11 - A\\Research 2\\R2X1-03_landuseproj\\test_imgdata\\satellite_4800x4800_unsorted'
+test_labelses = 'D:\\Users\\lwrnc\\Desktop\\LWRNC\\11 - A\\Research 2\\R2X1-03_landuseproj\\test_imgdata\\colormap_20x20_unsorted'
 
 image_gridifier.generate_sorted_grid_image_files_by_directory(
-    8,
-    images_directory=images,
-    labelses_directory=labelses,
+    images_directory=test_images,
+    labelses_directory=test_labelses,
     subimage_height=mp.img_HEIGHT,
     subimage_width=mp.img_WIDTH,
     lookup_colors=mp.lookup_rgb_to_index_full,
