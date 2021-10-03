59ec020fb7fba417d1f8dc8947adfa87a1f51351
pshs22-dominic-lawrence-bermudez <56672015+pshs22-dominic-lawrence-bermudez@users.noreply.github.com>
2021-10-04 03:53:07 +0800

    use Path.cwd() for relative directory traversal

--- main_gridifier.py
+++ main_gridifier.py
@@ -7,17 +7,37 @@ import main_params as mp
 
 # create local copy of main_gridifier.py and 
 
-train_images = (Path       # C:\\path\\to\\folder\\of\\large_satellite_images (Training Set)
-train_labelses = ''     # C:\\path\\to\\folder\\of\\colormaps (Training Set)
+train_images = str(
+    Path.cwd()
+    / 'datasets'
+    / 'train_images'
+    / 'satellite_4800x4800_unsorted'
+)
+train_labelses = str(
+    Path.cwd()
+    / 'datasets'
+    / 'train_images'
+    / 'colormap_20x20_unsorted'
+)
 
-test_images = ''        # C:\\path\\to\\folder\\of\\large_satellite_images (Test Set)
-test_labelses = ''      # C:\\path\\to\\folder\\of\\colormaps (Test Set)
+test_images = str(
+    Path.cwd()
+    / 'datasets'
+    / 'test_images'
+    / 'satellite_4800x4800_unsorted'
+)
+test_labelses = str(
+    Path.cwd()
+    / 'datasets'
+    / 'test_images'
+    / 'colormap_20x20_unsorted'
+)
 
 image_gridifier.generate_sorted_grid_image_files_by_directory(
-    images_directory=test_images,
-    labelses_directory=test_labelses,
-    subimage_height=mp.img_HEIGHT,
-    subimage_width=mp.img_WIDTH,
-    lookup_colors=mp.lookup_rgb_to_index_full,
-    label_names=mp.label_names_full
+    images_directory = test_images,
+    labelses_directory = test_labelses,
+    subimage_height = mp.img_HEIGHT,
+    subimage_width = mp.img_WIDTH,
+    lookup_colors = mp.lookup_rgb_to_index_full,
+    label_names = mp.label_names_full
 )
