1050139d257039914f96567a265118198ac4519e
pshs22-dominic-lawrence-bermudez <56672015+pshs22-dominic-lawrence-bermudez@users.noreply.github.com>
2022-01-18 22:57:40 +0800

    added a CLI "selection" for choosing which model to evaluate

--- main2_test.py
+++ main2_test.py
@@ -11,13 +11,20 @@ from pathlib import Path
 
 # --- ----
 
-models = [sub_file for sub_file in Path(mp.TRAINED_MODELS_DIRECTORY).iterdir() if sub_file.is_file()]
-chosen_model = models[14]
+model_file_paths = [sub_file for sub_file in Path(mp.TRAINED_MODELS_DIRECTORY).iterdir() if sub_file.is_file() and sub_file.suffix == '.hdf5']
+number_of_models = len(model_file_paths)
+
+print('Detected ' + str(number_of_models) + ' models in directory:')
+for i,model_file_path in enumerate(model_file_paths):
+    print('(' + str(i) + ') ' + str(model_file_path))
+
+model_index = int(input('\nWhich model will be evaluated? [0-' + str(number_of_models - 1) + ']: '))
+chosen_model = model_file_paths[model_index]
 
 # ---
 
 test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    mp.TEST_DATASET_DIRECTORY,
+    mp.TRAIN_DATASET_DIRECTORY,
     shuffle = True,
     
     labels = "inferred",
@@ -65,4 +72,10 @@ test_model.summary()
 results = test_model.evaluate(
     test_dataset,
     verbose=2
+)
+
+confusion_matrix = model_funcs.generate_confusion_matrix(
+    test_model,
+    mp.TRAIN_DATASET_DIRECTORY,
+    mp.label_names
 )
